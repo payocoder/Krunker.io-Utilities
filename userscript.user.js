cb81f5a8a337b6266d3a06c3364cf70e34a2dce5
Tehchy <juniortec21@yahoo.com>
2019-01-02 10:54:40 -0500

    0.4
    Added Skin Count to Account tab
    Added Autobanning to users who get banned from the Player List
    Added AntiGuest setting when enabled will auto kick guests
    Fixed issue with opening player list

--- userscript.user.js
+++ userscript.user.js
@@ -3,7 +3,7 @@
 // @description  Krunker.io Mod
 // @updateURL    https://github.com/Tehchy/Krunker.io-Utilities/raw/master/userscript.user.js
 // @downloadURL  https://github.com/Tehchy/Krunker.io-Utilities/raw/master/userscript.user.js
-// @version      0.3
+// @version      0.4
 // @author       Tehchy
 // @include      /^(https?:\/\/)?(www\.)?(.+)krunker\.io(|\/|\/\?(server|party)=.+)$/
 // @grant        GM_xmlhttpRequest
@@ -16,10 +16,19 @@ document.innerHTML = "";
 
 class Utilities {
     constructor() {
+        this.me = null;
+        this.camera = null;
+        this.inputs = null;
+        this.game = null;
+        this.customMatch = false;
+        this.hooks = {
+            socket: null,
+        };
         this.fps = {
             cur: 0,
             times: []
         };
+        this.banList = [];
         this.canvas = null;
         this.ctx = null;
         this.settings = {
@@ -33,6 +42,7 @@ class Utilities {
             customCrosshairLength: 14,
             customCrosshairThickness: 2,
             customCrosshairOutline: 0,
+            antiGuest: false,
         };
         this.settingsMenu = [];
         this.onLoad();
@@ -149,6 +159,17 @@ class Utilities {
                     self.settings.customCrosshairOutline = parseInt(t);
                 }
             },
+            antiGuest: {
+                name: "Auto Kick Guests",
+                pre: "<div class='setHed'>Custom Games</div>",
+                val: 0,
+                html() {
+                    return `<label class='switch'><input type='checkbox' onclick='window.utilities.setSetting("antiGuest", this.checked)' ${self.settingsMenu.antiGuest.val ? "checked" : ""}><span class='slider'></span></label>`;
+                },
+                set(t) {
+                    self.settings.antiGuest = t;
+                }
+            },
         };
     }
 
@@ -165,7 +186,11 @@ class Utilities {
 
     keyDown(event) {
         if (document.activeElement.id === 'chatInput') return;
-        if (event.keyCode === 9) window.showWindow(window.windows.length - 1);
+        if (event.keyCode === 9) {
+            document.exitPointerLock = document.exitPointerLock || document.mozExitPointerLock;
+            document.exitPointerLock();
+            window.showWindow(window.windows.length - 1);
+        }
     }
 
     keyUp(event) {
@@ -244,12 +269,50 @@ class Utilities {
     crosshairOpacity(t) {
         return this.settings.customCrosshair == 1 ? 0 : t;
     }
+    
+    countSkins(y) {
+        let d = 0;
+        for (let i = 0; i < y.skins.length; i++) {
+           d += y.skins[i].cnt - 1;
+        }
+        return "U:" + y.skins.length + ", D:" + d;
+    }
+    
+    autoBan() {
+        if (!this.customMatch) return
+        let autoBan = this.game.players.list.filter(p => this.banList.includes(p.name));
+        for (let player of autoBan) {
+            this.hooks.socket.send("c", "/ban " + player.name)
+        }
+    }
+    
+    antiGuest() {
+        if (!this.customMatch) return
+        if (!this.settings.antiGuest) return;
+        
+        let guests = this.game.players.list.filter(p => p.name.match(/Guest_([0-9]{1}|1[0-5])$/));
+        for (let player of guests) {
+            this.hooks.socket.send("c", "/kick " + player.name)
+        }
+    }
 
     render() {
         this.ctx.clearRect(0, 0, innerWidth, innerHeight);
         this.drawCrosshair();
         this.drawFPS();
         requestAnimationFrame(this.render.bind(this));
+    }
+    
+    loop(camera, me, inputs, game) {
+        this.me = me;
+        this.camera = camera;
+        this.game = game;
+        this.inputs = inputs;
+        
+        this.autoBan();
+        this.antiGuest();
+        
+        if (!this.customMatch) this.banList = [];
     }
 
     setSetting(t, e) {
@@ -283,8 +346,9 @@ GM_xmlhttpRequest({
         let game = /=(.)\.store\.purchases/.exec(code)[1];
         let sock = /return (.)\.send\("getMaps"\)/.exec(code)[1];
         code = code.replace(/String\.prototype\.escape=function\(\){(.*)\)},(Number\.)/, "$2")
-            .replace(/window\.updateWindow=function/, 'windows.push({header:"Player List",gen:function(){var t="<div style=\'margin-top:0px\' class=\'setHed\'><center>Player List</center></div><div class=\'settNameSmall\'><span class=\'floatR\'>Host Only</span></div>";for(let p of ' + game + '.players.list){t+="<div class=\'settName\'>"+p.name+(!p.isYou?"<span class=\'floatR\'><span id=\'kick\' class=\'settText\' onclick=\'userAction(0, &quot;"+p.id+"&quot;)\'>Kick</span> | <span id=\'ban\' class=\'settText\' onclick=\'userAction(1, &quot;"+p.id+"&quot;)\'>Ban</span></span>":"")+"</div>";}return t;}});windows.push({header: "Utilities", html: "",gen: function () {var t = ""; for (var key in window.utilities.settingsMenu) {window.utilities.settingsMenu[key].pre && (t += window.utilities.settingsMenu[key].pre), t += "<div class=\'settName\'>" + window.utilities.settingsMenu[key].name + " " + window.utilities.settingsMenu[key].html() + "</div>";} return t;}});window.utilities.setupSettings();\nwindow.updateWindow=function')
-            .replace(/window\.windows=/, 'window.userAction = function(type = 0, id) {let user = ' + game + '.players.list.filter(x => x.id == id);if(user){user = user[0];' + sock + '.send("c", "/" + (type == 0 ? "kick" : "ban") + " " + user.name);}},window.windows =')
+            .replace(/(\w+).procInputs\((\w+),(\w+)\),(\w+).moveCam/, 'window.utilities.loop($4, $1, $2, $3), $1.procInputs($2,$3),$4.moveCam')
+            .replace(/window\.updateWindow=function/, 'windows.push({header:"Player List",gen:function(){var t="<div style=\'margin-top:0px\' class=\'setHed\'><center>Player List</center></div><div class=\'settNameSmall\'><span class=\'floatR\'>Host Only</span></div>";for(let p of ' + game + '.players.list){t+="<div class=\'settName\'>"+p.name+(!p.isYou && window.utilities.customMatch?"<span class=\'floatR\'><span id=\'kick\' class=\'settText\' onclick=\'userAction(0, &quot;"+p.id+"&quot;)\'>Kick</span> | <span id=\'ban\' class=\'settText\' onclick=\'userAction(1, &quot;"+p.id+"&quot;)\'>Ban</span></span>":"")+"</div>";}return t;}});windows.push({header: "Utilities", html: "",gen: function () {var t = ""; for (var key in window.utilities.settingsMenu) {window.utilities.settingsMenu[key].pre && (t += window.utilities.settingsMenu[key].pre), t += "<div class=\'settName\'>" + window.utilities.settingsMenu[key].name + " " + window.utilities.settingsMenu[key].html() + "</div>";} return t;}});window.utilities.setupSettings();\nwindow.updateWindow=function')
+            .replace(/window\.windows=/, 'window.userAction = function(type = 0, id) {let user = ' + game + '.players.list.filter(x => x.id == id);if(user){user = user[0];if(type==1)window.utilities.banList.push(user.name);' + sock + '.send("c", "/" + (type == 0 ? "kick" : "ban") + " " + user.name);}},window.windows =')
             .replace(/window\.addEventListener\("keydown",function\((\w+)\){/, 'window.addEventListener("keydown",function($1){window.utilities.keyDown($1),')
             .replace(/window\.addEventListener\("keyup",function\((\w+)\){/, 'window.addEventListener("keyup",function($1){window.utilities.keyUp($1),')
             .replace(/window\.addEventListener\("keypress",function\((\w+)\){/, 'window.addEventListener("keypress",function($1){window.utilities.keyPress($1),')
@@ -294,6 +358,9 @@ GM_xmlhttpRequest({
             .replace(/antialias:!1/g, 'antialias:window.utilities.settings.antiAlias ? 1 : !1')
             .replace(/precision:"mediump"/g, 'precision:window.utilities.settings.highPrecision ? "highp": "mediump"')
             .replace(/crosshair\.style\.opacity\=(\w+)\)/, 'crosshair.style.opacity = window.utilities.crosshairOpacity($1))')
+            .replace(/\((\w+).timePlayed\)\+"/, '($1.timePlayed)+"</span></div><div class=\'settName\'>Skins<span class=\'floatR\'>"+window.utilities.countSkins($1)+"')
+            .replace(/if\(!this\.socket\){/, 'if(!this.socket){window.utilities.hooks.socket = this;')
+            .replace(/(if \((\w+) \? (\w+).data)/, 'window.utilities.customMatch = $2;$1')
             .replace(/setTimeout\(\(\)=>{!(.*)},2500\);/, '');
         GM_xmlhttpRequest({
             method: "GET",
